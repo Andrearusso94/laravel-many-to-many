da31841b350e25c46152cf5585fd4d8831ce74e1
Andrearusso94 <andrea.r.4845@gmail.com>
2023-01-17 15:27:01 +0100

    Add Controller, Models and folder pivot Technology

---
+++ app/Models/Technology.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+class Technology extends Model
+{
+    use HasFactory;
+    protected $table = 'technology';
+    /** 
+     * Get all of the posts for the category
+    @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
+    public function projects(): BelongsToMany
+    {
+        return $this->belongsToMany(Project::class);
+    }
+}
