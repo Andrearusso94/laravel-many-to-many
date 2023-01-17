9c26f548d2e12cbea626a19b49b8867ef91cabb5
Andrearusso94 <andrea.r.4845@gmail.com>
2023-01-17 16:57:38 +0100

    add show

--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -3,10 +3,20 @@
 @section('content')
 
 <h1>{{$project->title}}</h1>
-<h4>{{$project->type->name}}</h4>
+<h4>{{$project->type ? $project->type->name : 'undefined'}}</h4>
 <h5>{{$project->slug}}</h5>
 <div class="content">
-    {{$project->body}}
+    <p>{{$project->body}}</p>
+
+    @if(count($project->technologys) > 0)
+
+    @foreach($project->technologys as $technology)
+    <p>{{$technology->name}}</p>
+    @endforeach
+
+    @else
+    <span>not technology</span>
+    @endif
 </div>
 
 
